cacd79dbf5b895033e119e58c3696606208ee90b
Sandeep Kamarajugadda <sandeep.kamarajugadda@spx.com>
2021-04-21 00:22:39 +0530

    Updated
    Updated

--- Sources/Wyseplus/Wyseplus.swift
+++ Sources/Wyseplus/Wyseplus.swift
@@ -10,7 +10,7 @@ public class NetworkingCalls: NSObject {
     
     let baseURL = "http://api.randomuser.me/"
     
-    func getAccessToken(onCompletion : @escaping (String) -> Void) {
+   public func getAccessToken(onCompletion : @escaping (String) -> Void) {
         networking.post("http://52.53.168.217:8080/oauth/token?grant_type=client_credentials", parameters: ["client_id" : "phanitest"]) { result in
             switch result
             {
