67df7b2f8a4e69887d15a794ff8bd6e8f242b7fc
Sandeep Kamarajugadda <sandeep.kamarajugadda@spx.com>
2021-04-21 00:19:52 +0530

    Updated
    Update

--- Sources/Wyseplus/Wyseplus.swift
+++ Sources/Wyseplus/Wyseplus.swift
@@ -2,33 +2,23 @@ import Foundation
 import UIKit
 import WebKit
 
-public class MediumWebView: WKWebView {
-    
-    public init() {
-        let webConfiguration = WKWebViewConfiguration()
-        webConfiguration.preferences.javaScriptEnabled = true
-        super.init(frame: .zero, configuration: webConfiguration)
-        self.scrollView.isScrollEnabled = false
-        self.isMultipleTouchEnabled = false
-    }
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
-    public func loadRe() {
-      print("Test",getRequest())
-    }
-    
+public class NetworkingCalls: NSObject {
     lazy var networking: Networking = {
         let networking = Networking(baseURL: "http://52.53.168.217:8080")
         return networking
     }()
     
-    public func getRequest()
-    {
-        networking.post("/oauth/token?grant_type=client_credentials", parameters: ["client_id" : "phanitest"]) { result in
-            
-            print("Sandeeep")
+    let baseURL = "http://api.randomuser.me/"
+    
+    func getAccessToken(onCompletion : @escaping (String) -> Void) {
+        networking.post("http://52.53.168.217:8080/oauth/token?grant_type=client_credentials", parameters: ["client_id" : "phanitest"]) { result in
+            switch result
+            {
+            case .success( _):
+                onCompletion("Success")
+            case .failure(_):
+                onCompletion("Failure")
             }
-        print("Sasdsdasdas")
+        }
     }
 }
