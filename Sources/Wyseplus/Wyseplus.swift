774d6e37f360024b1fd24d27bf5c0574481bc8ef
Sandeep Kamarajugadda <sandeep.kamarajugadda@spx.com>
2021-04-21 10:03:34 +0530

    Updated
    Updated

--- Sources/Wyseplus/Wyseplus.swift
+++ Sources/Wyseplus/Wyseplus.swift
@@ -8,17 +8,18 @@ public class NetworkingCalls: NSObject {
         return networking
     }()
     
-    let baseURL = "http://api.randomuser.me/"
     
-   public func getAccessToken(onCompletion : @escaping (String) -> Void) {
-        networking.post("http://52.53.168.217:8080/oauth/token?grant_type=client_credentials", parameters: ["client_id" : "phanitest"]) { result in
-            switch result
-            {
-            case .success( _):
-                onCompletion("Success")
-            case .failure(_):
-                onCompletion("Failure")
+    let baseURL = "http://api.randomuser.me/"
+
+        func getAccessToken(onCompletion : @escaping (String) -> ()) {
+            networking.post("http://52.53.168.217:8080/oauth/token?grant_type=client_credentials", parameters: ["client_id" : "phanitest"]) { result in
+                switch result
+                            {
+                            case .success( _):
+                                onCompletion("Success")
+                            case .failure(_):
+                                onCompletion("Failure")
+                }
             }
         }
-    }
 }
