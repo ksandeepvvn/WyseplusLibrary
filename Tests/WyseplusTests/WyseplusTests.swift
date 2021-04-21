bf427eb42de300e75f5e5341a9234f1aa244e392
Sandeep Kamarajugadda <sandeep.kamarajugadda@spx.com>
2021-04-21 10:19:47 +0530

    Updated
    Updated

--- Tests/WyseplusTests/WyseplusTests.swift
+++ Tests/WyseplusTests/WyseplusTests.swift
@@ -6,7 +6,6 @@ final class WyseplusTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Wyseplus().text, "Hello, World!")
     }
 
     static var allTests = [
