342983f45bd152f866609ec1d2a7d8f46a534c41
Sandeep Kamarajugadda <sandeep.kamarajugadda@spx.com>
2021-04-21 10:14:03 +0530

    Updated
    Updated

--- Sources/Wyseplus/Wyseplus.swift
+++ Sources/Wyseplus/Wyseplus.swift
@@ -7,11 +7,8 @@ public class NetworkingCalls: NSObject {
         let networking = Networking(baseURL: "http://52.53.168.217:8080")
         return networking
     }()
-    
-    
-    let baseURL = "http://api.randomuser.me/"
-
        public func getAccessToken(onCompletion : @escaping (String) -> ()) {
+        networking.setAuthorizationHeader(username: "phanitest", password: "testrandom123")
             networking.post("http://52.53.168.217:8080/oauth/token?grant_type=client_credentials", parameters: ["client_id" : "phanitest"]) { result in
                 switch result
                             {
