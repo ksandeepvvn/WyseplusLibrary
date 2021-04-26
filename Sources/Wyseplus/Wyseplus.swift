272da0c481f35e20c7330833d0ce6def2e3240f7
Sandeep Kamarajugadda <sandeep.kamarajugadda@spx.com>
2021-04-26 11:02:35 +0530

    Updated

--- Sources/Wyseplus/Wyseplus.swift
+++ Sources/Wyseplus/Wyseplus.swift
@@ -8,22 +8,31 @@ public class NetworkingCalls: NSObject {
         let networking = Networking(baseURL: Constants.BASE_URL)
         return networking
     }()
-       public func getAccessToken(onCompletion : @escaping (String) -> ()) {
-        networking.setAuthorizationHeader(username: "phanitest", password: "testrandom123")
-        networking.post(Constants.OAUTH_URL, parameters: [Constants.ClIENTID : "phanitest"]) { result in
-                switch result
-                            {
-                            case .success( _):
-                                self.defaults.set("ABC", forKey: Constants.ACCESS_TOKEN)
-                                onCompletion(Constants.SUCCESS_RESPONSE)
-                            case .failure(_):
-                                self.defaults.set("ABC", forKey: Constants.ACCESS_TOKEN)
-                                onCompletion(Constants.FAILURE_RESPONSE)
+       public func generateAccessToken(onCompletion : @escaping (String) -> ()) {
+        let request = NSMutableURLRequest(url: NSURL(string: Constants.TRACK_EVENT)! as URL)
+        request.setValue("Basic \(getLoginCredentialsAuthValue(username: "phanitest", password: "testrandom123"))", forHTTPHeaderField: "Authorization")
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpMethod = "POST"
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: [] as Any, options: []) else {
+               return
+           }
+           request.httpBody = httpBody
+           let session = URLSession.shared
+            session.dataTask(with: request as URLRequest) { (data, response, error) in
+               if let response = response {
+                guard let httpResponse = response as? HTTPURLResponse else {return}
+                switch httpResponse.statusCode {
+                case 200:
+                    onCompletion(Constants.SUCCESS_RESPONSE)
+                default:
+                    onCompletion(Constants.FAILURE_RESPONSE)
                 }
-            }
+               }
+           }.resume()
         }
     
-    public func getLoginToken(username: String, password: String) -> String
+    public func getLoginCredentialsAuthValue(username: String, password: String) -> String
     {
         let credentialsString = "\(username):\(password)"
         var authString : String = ""
@@ -45,7 +54,7 @@ public class NetworkingCalls: NSObject {
     
     public func postEventTrigger(params: [[String : String]]?,onCompletion : @escaping (String) -> ()) {
         let request = NSMutableURLRequest(url: NSURL(string: Constants.TRACK_EVENT)! as URL)
-        request.setValue("Bearer \("PFqGRrSTcXUPOGTZFhXY7Jd-V0k")", forHTTPHeaderField: "Authorization")
+        request.setValue("Bearer \(getAccessToken())", forHTTPHeaderField: "Authorization")
         request.setValue("phanitest", forHTTPHeaderField: Constants.TENANT_NAME)
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue("application/json", forHTTPHeaderField: "Accept")
@@ -65,15 +74,6 @@ public class NetworkingCalls: NSObject {
                     onCompletion(Constants.FAILURE_RESPONSE)
                 }
                }
-//               if let data = data {
-//                   do {
-//                       let json = try JSONSerialization.jsonObject(with: data, options: [])
-//                       print(json)
-//                   } catch {
-//                       print(error)
-//                   }
-//               }
            }.resume()
-        
      }
 }
